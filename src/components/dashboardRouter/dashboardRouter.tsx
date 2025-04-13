b617598d5b8fc75e93954b82d9e28db96c3847c3
ridham <ridhamsuhagiya@gmail.com>
2025-04-13 19:13:46 +0530

    backend redirect url changes added

--- src/components/dashboardRouter/dashboardRouter.tsx
+++ src/components/dashboardRouter/dashboardRouter.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../../views/dashboard/dashboard";
 import UserProfile from "../../views/profile/profile";
 import { dashboard, feed, login, profile } from "../../constants/variable";
 import InstagramLogin from "../../views/login/login";
 import Feeds from "../../views/feeds/feeds";
+import { getUserId } from "../../session";
 
 const ChattinSystem = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to={getUserId() ? `/${dashboard}` : `/${login}`} />} />
                 <Route path={`/${login}`} element={<InstagramLogin />} />
                 <Route path={`/${dashboard}`} element={<Dashboard />}>
                     <Route path={`${feed}`} element={<Feeds />} />
